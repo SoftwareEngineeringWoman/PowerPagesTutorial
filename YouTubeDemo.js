72c576caf0a89ff2878b9656ede979b57d650492
Saira Walia <mssairawalia@gmail.com>
2024-03-14 23:13:30 -0700

    New

--- YouTubeDemo.js
+++ YouTubeDemo.js
@@ -36,6 +36,8 @@ function startUpProcedure() {
 
 
 // *** Document Ready - Load Power Pages WEBAPI support    .
+// *** Document Ready - Load Power Pages WEBAPI support    .
+
 $( document ).ready(function() {
 
     (function(webapi, $){
